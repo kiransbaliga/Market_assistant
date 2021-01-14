642f33262a35c93fd2e4fb790740942410b21286
Athul John <athuljohn@ieee.org>
2021-01-14 17:19:40 +0530

    improved itemdetail page

--- client/lib/Objects/itemclass.dart
+++ client/lib/Objects/itemclass.dart
@@ -4,4 +4,38 @@ class Item {
   double price;
   double cost;
   String image;
+  String companyUrl = '';
+  Map<double, double> rating = {1: 0, 2: 0, 3: 0, 4: 1, 5: 0};
+  Map<String, Map<String, String>> specs = {
+    'Taste': {
+      'Sweet': 'Yes',
+      'Bitter': 'Yes',
+      'Salty': 'No',
+    },
+    'Package': {
+      'Material': 'Plastic',
+      'Sealing': 'Regular',
+      'Resealable': 'No',
+    },
+    'Ingredients': {'Almonds': 'NO', 'Nuts': 'Yes'}
+  };
+  double getrating() {
+    double rat = 0, no = 0;
+    for (double i in rating.keys) {
+      rat += (i * rating[i]);
+      no += rating[i];
+    }
+    if (no == 0) return 0;
+    return rat / no;
+  }
+
+  double gettotal() {
+    double no = 0;
+    for (double i in rating.keys) {
+      no += rating[i];
+    }
+
+    if (no == 0) return 0;
+    return no;
+  }
 }
--- client/lib/Screens/itemDetails.dart
+++ client/lib/Screens/itemDetails.dart
@@ -1,9 +1,17 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:markus/Objects/itemclass.dart';
 
 class ItemDetail extends StatelessWidget {
   final Item _item;
   ItemDetail(this._item);
+  Color getcolor() {
+    double d = _item.getrating();
+    if (d < 2) return Colors.red;
+    if (d < 3.5) return Colors.orange;
+    return Colors.green;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,11 +27,11 @@ class ItemDetail extends StatelessWidget {
         ),
       ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 15),
+        padding: const EdgeInsets.symmetric(horizontal: 23),
         child: ListView(
           children: [
             Container(
-                height: MediaQuery.of(context).size.width / 1.5,
+                height: MediaQuery.of(context).size.width / 1.75,
                 child: Center(
                   child: Image.network(
                     _item.image,
@@ -38,7 +46,7 @@ class ItemDetail extends StatelessWidget {
               text: TextSpan(
                 children: [
                   TextSpan(
-                    text: '\n ₹${_item.price.floor()} ',
+                    text: '\n₹${_item.price.floor()} ',
                     style: TextStyle(fontSize: 30, color: Colors.black),
                   ),
                   TextSpan(
@@ -50,7 +58,7 @@ class ItemDetail extends StatelessWidget {
                   ),
                   TextSpan(
                     text:
-                        '    ${(((_item.cost - _item.price) / _item.cost) * 100).ceil()}% OFF!',
+                        '  ${(((_item.cost - _item.price) / _item.cost) * 100).ceil()}% OFF!',
                     style: TextStyle(fontSize: 20, color: Colors.green),
                   ),
                 ],
@@ -61,37 +69,191 @@ class ItemDetail extends StatelessWidget {
             ),
             Row(
               children: [
-                FlatButton(
-                  color: Colors.green,
-                  onPressed: () {},
-                  child: Text('Buy Now'),
+                Expanded(
+                  child: RaisedButton(
+                    padding: EdgeInsets.symmetric(vertical: 8),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(7)),
+                    color: Colors.grey[200],
+                    onPressed: () {},
+                    child: Text(
+                      'Add to Cart',
+                      style: TextStyle(fontSize: 20, color: Colors.black),
+                    ),
+                  ),
                 ),
-                FlatButton(
-                  onPressed: () {},
-                  child: Text('Add to Cart'),
+                SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: FlatButton(
+                    padding: EdgeInsets.symmetric(vertical: 8),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(7)),
+                    color: Colors.green,
+                    onPressed: () {},
+                    child: Text(
+                      'Buy ',
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                  ),
                 ),
               ],
             ),
-            SizedBox(
-              height: 20,
-            ),
-            FlatButton(
-              color: Colors.green,
-              minWidth: double.infinity,
-              onPressed: () {},
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Official Site of the Brand'),
-                  Icon(Icons.launch),
-                ],
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+              child: RaisedButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5)),
+                padding: EdgeInsets.all(8),
+                color: Colors.grey[200],
+                // width: double.infinity,
+                onPressed: () {},
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('View Product Ad'),
+                    Icon(Icons.launch),
+                  ],
+                ),
               ),
             ),
-            Row(
-              children: [
-                Text('Customer Ratings'),
-              ],
-            )
+            _item.getrating() == 0
+                ? Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                        Text(
+                          'Customer Ratings',
+                          style: TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                        Text('No ratings till Now...')
+                      ])
+                : Container(
+                    // height: 250,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Column(
+                          children: [
+                            Text(
+                              'Customer Ratings',
+                              style: TextStyle(
+                                fontSize: 18,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Container(
+                              padding: EdgeInsets.symmetric(horizontal: 8),
+                              decoration: BoxDecoration(
+                                  color: getcolor(),
+                                  borderRadius: BorderRadius.circular(10)),
+                              child: Row(
+                                children: [
+                                  Icon(Icons.star, color: Colors.white),
+                                  Text(
+                                    '${_item.getrating().toStringAsFixed(1)}',
+                                    style: TextStyle(
+                                        fontSize: 25, color: Colors.white),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Text(
+                              'from ${_item.gettotal().floor()} ratings',
+                              style:
+                                  TextStyle(fontSize: 15, color: Colors.grey),
+                            )
+                          ],
+                        ),
+                        Expanded(
+                          child: PieChart(PieChartData(
+                              borderData: FlBorderData(show: false),
+                              centerSpaceRadius: 30,
+                              sections: [
+                                PieChartSectionData(
+                                    color: Colors.red,
+                                    title: ' ',
+                                    value: _item.rating[1]),
+                                PieChartSectionData(
+                                    color: Colors.orange,
+                                    title: ' ',
+                                    value: _item.rating[2]),
+                                PieChartSectionData(
+                                    color: Colors.yellow,
+                                    title: ' ',
+                                    value: _item.rating[3]),
+                                PieChartSectionData(
+                                    color: Colors.lightGreen,
+                                    title: ' ',
+                                    value: _item.rating[4]),
+                                PieChartSectionData(
+                                    color: Colors.green,
+                                    title: ' ',
+                                    value: _item.rating[5]),
+                              ])),
+                        )
+                      ],
+                    ),
+                  ),
+            if (_item.specs.keys.length != 0)
+              Padding(
+                padding: const EdgeInsets.only(bottom: 10, top: 10),
+                child: Text(
+                  'Specifications',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            Container(
+                padding: EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(10)),
+                child: Column(
+                  children: [
+                    for (String s in _item.specs.keys)
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            s,
+                            style: TextStyle(
+                                fontSize: 19, fontWeight: FontWeight.bold),
+                          ),
+                          for (String sr in _item.specs[s].keys)
+                            Padding(
+                              padding: const EdgeInsets.all(5.0),
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                    child: Text(
+                                      sr,
+                                      style: TextStyle(fontSize: 17),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: Text(
+                                      _item.specs[s][sr],
+                                      style: TextStyle(
+                                          fontSize: 17,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          Divider(
+                            thickness: 1,
+                          )
+                        ],
+                      )
+                  ],
+                ))
           ],
         ),
       ),
